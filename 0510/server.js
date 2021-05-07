563ee4cee92516d155a089b3b678e1c0033642b3
hyejjun <hjlee9725@gmail.com>
2021-05-07 19:19:42 +0900

    bug

--- 0510/server.js
+++ 0510/server.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 });
 
+app.get('/login',(req,res)=>{
+    res.setD('asdfasdf')
+});
+
 app.listen(3000,()=>{
     console.log('server start port 3000');
 });
